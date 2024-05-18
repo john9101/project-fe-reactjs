869c6bd72ecf626c06d958d10891bdc48d9c82dc
john9101 <21130319@st.hcmuaf.edu.vn>
2024-05-18 17:00:21 +0700

    set up base component

---
+++ src/components/filter/PriceFilter.tsx
@@ -0,0 +1,46 @@
+const PriceFilter = () => {
+    return (
+        <div className="border-bottom mb-4 pb-4">
+            <h5 className="font-weight-semi-bold mb-4">Filter by price</h5>
+            <form>
+                <div
+                    className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                    <input type="checkbox" className="custom-control-input" checked id="price-all"/>
+                    <label className="custom-control-label" htmlFor="price-all">All Price</label>
+                    <span className="badge border font-weight-normal">1000</span>
+                </div>
+                <div
+                    className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                    <input type="checkbox" className="custom-control-input" id="price-1"/>
+                    <label className="custom-control-label" htmlFor="price-1">$0 - $100</label>
+                    <span className="badge border font-weight-normal">150</span>
+                </div>
+                <div
+                    className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                    <input type="checkbox" className="custom-control-input" id="price-2"/>
+                    <label className="custom-control-label" htmlFor="price-2">$100 - $200</label>
+                    <span className="badge border font-weight-normal">295</span>
+                </div>
+                <div
+                    className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                    <input type="checkbox" className="custom-control-input" id="price-3"/>
+                    <label className="custom-control-label" htmlFor="price-3">$200 - $300</label>
+                    <span className="badge border font-weight-normal">246</span>
+                </div>
+                <div
+                    className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                    <input type="checkbox" className="custom-control-input" id="price-4"/>
+                    <label className="custom-control-label" htmlFor="price-4">$300 - $400</label>
+                    <span className="badge border font-weight-normal">145</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between">
+                    <input type="checkbox" className="custom-control-input" id="price-5"/>
+                    <label className="custom-control-label" htmlFor="price-5">$400 - $500</label>
+                    <span className="badge border font-weight-normal">168</span>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default PriceFilter
