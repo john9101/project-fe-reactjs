4e27d5fffc11af720aa1cb64312b83a959bdbf59
john9101 <21130319@st.hcmuaf.edu.vn>
2024-05-24 18:01:20 +0700

    config bare router for react project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import './App.css';
 import './assets/css/style.css';
-import {BrowserRouter} from "react-router-dom";
-import Topbar from "./components/common/Topbar";
-import CartRouter from "./router/CartRouter";
-import Footer from "./components/common/Footer";
-import ContactUsRouter from "./router/ContactUsRouter";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainLayout from "./pages/MainLayout";
+// import CartDetail from "./components/cart/CartDetail";
+// import ContactUs from "./pages/ContactUs";
+import {CircularProgress} from "@mui/material";
+
+const ContactUs = lazy(() => import('./pages/ContactUs'));
+const CartDetail = lazy(() => import('./components/cart/CartDetail'));
 
 function App() {
     return (
-
         <BrowserRouter>
             <div className="App">
-                <Topbar/>
-                <CartRouter/>
-                <ContactUsRouter/>
-                <Footer/>
+                <Suspense fallback={<CircularProgress color="success" />}>
+                    <Routes>
+                        <Route path="/" element={<MainLayout/>}>
+                            <Route path="/cart" element={<CartDetail/>}/>
+                            <Route path="/contact-us" element={<ContactUs/>}/>
+                            {/*Muốn trang có topbar, footer thì thêm một route vô đây*/}
+                        </Route>
+
+                        {/*<Route path="/any-router" element={<AnyComponent/>}> Muốn trang không có topbar, footer thì khai báo một route mới */}
+                    </Routes>
+                </Suspense>
             </div>
         </BrowserRouter>
     );
