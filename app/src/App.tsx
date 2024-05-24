407b9397aeefca3146864eb30772414e838d8f89
john9101 <21130319@st.hcmuaf.edu.vn>
2024-05-24 14:06:08 +0700

    repare App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import "./assets/css/style.css"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-        <FontAwesomeIcon icon={faCoffee} />
+
     </div>
   );
 }
