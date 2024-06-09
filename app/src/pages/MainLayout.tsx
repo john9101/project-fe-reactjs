00f22ded2632758314724b9a2376630f25ea8b0b
NguyenHoangAnhDuong <127371632+NguyenHoangAnhDuong@users.noreply.github.com>
2024-06-09 15:42:02 +0700

    pull code in branch main

--- app/src/pages/MainLayout.tsx
+++ app/src/pages/MainLayout.tsx
@@ -1,15 +1,18 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Topbar from "../components/common/Topbar";
 import Footer from "../components/common/Footer";
 import React from "react";
+import Navbar from "../components/common/Navbar";
 
 const MainLayout = () => {
 
     return (
         <>
-            <Topbar/>
-            <Outlet/>
-            <Footer/>
+            <Topbar />
+            <Navbar />
+            <Outlet />
+            <Footer />
+
         </>
     )
 }
