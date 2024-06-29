2ba7da87a4254168f04580b7a9b344d56729ace3
ThanhBinh2219 <21130291@st.hcmuaf.edu.vn>
2024-06-29 12:11:01 +0700

    feat: validate form input

--- app/src/components/contact/FormContact.tsx
+++ app/src/components/contact/FormContact.tsx
@@ -12,11 +12,29 @@ function FormContact() {
         topic: "",
         message: ""
     });
+    const [errors, setErrors] = useState({
+        username: "",
+        email: "",
+        topic: "",
+        message: ""
+    });
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const validateFields = () => {
+        const errors = {
+            username: contact.username ? "" : "* Vui lòng nhập họ và tên",
+            email: contact.email ? "" : "* Vui lòng nhập Email của bạn",
+            topic: contact.topic ? "" : "* Vui lòng nhập chủ đề cần liên hệ",
+            message: contact.message ? "" : "* Vui lòng nhập nội dung liên hệ"
+        };
+        setErrors(errors);
+        return !Object.values(errors).some(error => error !== "");
+    };
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { id, value } = e.target;
+        const {id, value} = e.target;
         setContact({
             ...contact,
             [id]: value
@@ -24,6 +42,9 @@ function FormContact() {
     };
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
+        if (!validateFields()) {
+            return;
+        }
         try {
             const response = await axios.post('http://localhost:4000/api/contact', contact);
             console.log('Đã lưu:', response.data);
@@ -49,20 +70,21 @@ function FormContact() {
                                required data-validation-required-message="Vui lòng nhập họ và tên"
                                value={contact.username}
                                onChange={handleChange}/>
-                        <p className="help-block text-danger"></p>
+                        <p className="help-block text-danger">{errors.username}</p>
                     </div>
                     <div className="control-group">
                         <input type="email" className="form-control" id="email" placeholder="Email"
-                               required data-validation-required-message="Vui lòng nhập Email của bạn" value={contact.email}
+                               required data-validation-required-message="Vui lòng nhập Email của bạn"
+                               value={contact.email}
                                onChange={handleChange}/>
-                        <p className="help-block text-danger"></p>
+                        <p className="help-block text-danger">{errors.email}</p>
                     </div>
                     <div className="control-group">
                         <input type="text" className="form-control" id="topic" placeholder="Chủ đề"
                                required data-validation-required-message="Vui lòng nhập chủ đề cần liên hệ"
                                value={contact.topic}
                                onChange={handleChange}/>
-                        <p className="help-block text-danger"></p>
+                        <p className="help-block text-danger">{errors.topic}</p>
                     </div>
                     <div className="control-group">
                             <textarea className="form-control" rows={6} id="message" placeholder="Nội dung"
@@ -70,9 +92,14 @@ function FormContact() {
                                       data-validation-required-message="Vui lòng nhập nội dung liên hệ"
                                       value={contact.message}
                                       onChange={handleChange}></textarea>
-                        <p className="help-block text-danger"></p>
+                        <p className="help-block text-danger">{errors.message}</p>
                     </div>
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'column'
+                    }}>
                         <button className="btn btn-primary py-2 px-4 rounded" type="submit" id="sendMessageButton">Gửi
                         </button>
                     </div>
@@ -86,10 +113,10 @@ function FormContact() {
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </Modal.Header>
-                <Modal.Body>Chúng tôi chân thành cảm ơn bạn.</Modal.Body>
+                <Modal.Body>Chúng tôi cảm ơn bạn đã liên hệ, chúng tôi sẽ phản hồi sớm nhất.</Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
-                       <NavLink to={"/"} style={{color: "black"}}> Trang chủ</NavLink>
+                        <NavLink to={"/"} style={{color: "black"}}> Trang chủ</NavLink>
                     </Button>
                     <Button variant="secondary" onClick={handleClose} style={{color: "black"}}>
                         Tiếp tục
@@ -102,3 +129,4 @@ function FormContact() {
 }
 
 export default FormContact;
+
