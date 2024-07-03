c35fceeae41bfdba86ac93e72b24ed7eb7789ce2
ThanhBinh2219 <21130291@st.hcmuaf.edu.vn>
2024-07-03 10:52:42 +0700

    refactor: update the payment method

--- app/src/components/cart/InformationCart.tsx
+++ app/src/components/cart/InformationCart.tsx
@@ -1,11 +1,28 @@
-// @ts-ignore
-import React, {useMemo} from "react";
-import {useSelector} from "react-redux";
-import {RootState} from "../../store/store";
-import {formatCurrency} from "../../util/formatCurrency";
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { formatCurrency } from "../../util/formatCurrency";
 
 export default function InformationCart() {
     const cart = useSelector((state: RootState) => state.cart);
+
+    const totalPrice = useMemo(() => {
+        return cart.cartItems.reduce((total, cartItem) => {
+            return total + cartItem.product.originalPrice * cartItem.quantity * (1 - cartItem.product.discountPercent);
+        }, 0);
+    }, [cart.cartItems]);
+
+    const totalDiscount = useMemo(() => {
+        return cart.cartItems.reduce((total, cartItem) => {
+            return total + cartItem.product.originalPrice * cartItem.quantity * cartItem.product.discountPercent;
+        }, 0);
+    }, [cart.cartItems]);
+
+    const totalPriceWithoutDiscount = useMemo(() => {
+        return cart.cartItems.reduce((total, cartItem) => {
+            return total + cartItem.product.originalPrice * cartItem.quantity;
+        }, 0);
+    }, [cart.cartItems]);
 
     return (
         <div className="col-lg-4">
@@ -17,34 +34,36 @@ export default function InformationCart() {
                     </div>
                 </div>
             </form>
-            {cart.cartItems?.map((cartItem) => (
-                <div className="card border-secondary mb-5">
-                    <div className="card-header bg-secondary border-0">
-                        <h4 className="font-weight-semi-bold m-0">Giỏ hàng</h4>
+            <div className="card border-secondary mb-5">
+                <div className="card-header bg-secondary border-0">
+                    <h4 className="font-weight-semi-bold m-0">Giỏ hàng</h4>
+                </div>
+                <div className="card-body">
+                    <div className="d-flex justify-content-between mb-3 pt-1">
+                        <h6 className="font-weight-medium">Giá trước giảm giá</h6>
+                        <h6 className="font-weight-medium">{formatCurrency(totalPriceWithoutDiscount)}</h6>
                     </div>
-                    <div className="card-body">
-                        <div className="d-flex justify-content-between mb-3 pt-1">
-                            <h6 className="font-weight-medium">Giá</h6>
-                            <h6 className="font-weight-medium">{formatCurrency(cart.totalPrice)}</h6>
-                        </div>
-                        <div className="d-flex justify-content-between mb-3 pt-1">
-                            <h6 className="font-weight-medium">Giảm</h6>
-                            <h6 className="font-weight-medium">{}</h6>
-                        </div>
-                        <div className="d-flex justify-content-between">
-                            <h6 className="font-weight-medium">Phí giao hàng </h6>
-                            <h6 className="font-weight-medium">0</h6>
-                        </div>
+                    <div className="d-flex justify-content-between mb-3 pt-1">
+                        <h6 className="font-weight-medium">Giảm giá</h6>
+                        <h6 className="font-weight-medium">{formatCurrency(totalDiscount)}</h6>
                     </div>
-                    <div className="card-footer border-secondary bg-transparent">
-                        <div className="d-flex justify-content-between mt-2">
-                            <h5 className="font-weight-bold">Tổng tiền</h5>
-                            <h5 className="font-weight-bold">{formatCurrency(cart.totalPrice - cartItem.quantity * cartItem.product.originalPrice * cartItem.product.discountPercent)}</h5>
-                        </div>
-                        <button className="btn btn-block btn-primary my-3 py-3">Đặt hàng</button>
+                    <div className="d-flex justify-content-between mb-3 pt-1">
+                        <h6 className="font-weight-medium">Giá sau giảm giá</h6>
+                        <h6 className="font-weight-medium">{formatCurrency(totalPrice)}</h6>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                        <h6 className="font-weight-medium">Phí giao hàng</h6>
+                        <h6 className="font-weight-medium">0</h6>
                     </div>
                 </div>
-            ))}
+                <div className="card-footer border-secondary bg-transparent">
+                    <div className="d-flex justify-content-between mt-2">
+                        <h5 className="font-weight-bold">Tổng tiền</h5>
+                        <h5 className="font-weight-bold">{formatCurrency(totalPrice)}</h5>
+                    </div>
+                    <button className="btn btn-block btn-primary my-3 py-3">Đặt hàng</button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
