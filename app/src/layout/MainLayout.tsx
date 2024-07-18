ec855f2984b3692f6de3afaa60c4b0e4741567b0
john9101 <21130319@st.hcmuaf.edu.vn>
2024-07-19 02:16:11 +0700

    handle show Breadcrumbs section

--- app/src/layout/MainLayout.tsx
+++ app/src/layout/MainLayout.tsx
@@ -1,21 +1,38 @@
-import { Outlet } from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import Topbar from "../components/common/Topbar";
 import Footer from "../components/common/Footer";
-import React from "react";
+import {useEffect, useState} from "react";
 import Navbar from "../components/common/Navbar";
+import BreadcrumbsSection from "../components/common/BreadcrumbsSection";
 
 const MainLayout = () => {
+    const [destBreadcrumb, setDestBreadcrumb] = useState<string | null>(null);
+    const {pathname} = useLocation()
+
+    useEffect(() => {
+        if(pathname.includes("products")){
+            setDestBreadcrumb("Chi tiết sản phẩm")
+        }else if(pathname.includes("about-us")){
+            setDestBreadcrumb("Giới thiệu")
+        }else if(pathname.includes('contact-us')){
+            setDestBreadcrumb("Liên hệ")
+        }
+    }, [pathname])
 
     return (
         <>
-            <Topbar />
-            <Navbar />
-            <Outlet />
-            <Footer />
+            <Topbar/>
+            <Navbar/>
+            {
+                pathname !== '/' && !pathname.includes('feature') && !pathname.includes('category') &&
+                !pathname.includes('shop') && !pathname.includes('search') &&
+                <BreadcrumbsSection destBreadcrumb={destBreadcrumb!}/>
+            }
+            <Outlet/>
+            <Footer/>
         </>
     )
 }
-
 
 
 export default MainLayout
